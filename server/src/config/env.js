9e4bbfa08b3fd636567730819041098ac0e89b00
Avi056 <Aviator05m@gmail.com>
2026-06-02 12:17:58 -0400

    starting netlify url changes

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -10,7 +10,8 @@ export const env = {
   googleClientId: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   googleCallbackUrl:
-    process.env.GOOGLE_CALLBACK_URL || "http://localhost:5001/auth/google/callback",
+    // process.env.GOOGLE_CALLBACK_URL || "http://localhost:5001/auth/google/callback",
+    process.env.GOOGLE_CALLBACK_URL || "https://fridgecraft.netlify.app/auth/google/callback",
   spoonacularApiKey: process.env.SPOONACULAR_API_KEY,
   pineconeApiKey: process.env.PINECONE_API_KEY,
   pineconeIndexName: process.env.PINECONE_INDEX_NAME || "fridgechef",
